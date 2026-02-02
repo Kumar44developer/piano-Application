858745a21bb9c64c82fe510921084736293d0f10
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2026-02-02 17:49:19 +0530

    Added click event listener to all keys

--- script.js
+++ script.js
@@ -3,3 +3,5 @@ const keys = document.querySelectorAll(".key");
 const whiteKeys = document.querySelectorAll(".key.white");
 
 const blackKeys = document.querySelectorAll(".key.black");
+
+keys.forEach(key => key.addEventListener("click",handleKeyClick));
